1552fffa411716896dc4bdfbb56a545ba991efbd
syamkoteswar <syamkoteswar22@gmail.com>
2020-02-24 17:05:44 +0530

    comiit

--- Demo Logger/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.*;
-
-public class Main {
-    private static Logger LOG=LogManager.getLogger(Main.class.getName());
-    public static void main(String[] args){
-        LOG.trace("hellooooo");
-    }
-}
--- Demo2/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.apache.logging.log4j.*;
-import org.apache.logging.log4j.LogManager;
-
-public class Main {
-    private static Logger LOG=LogManager.getLogger(Main.class);
-    public static void main(String[] args){
-        LOG.debug("hellooooo");
-    }
-}
